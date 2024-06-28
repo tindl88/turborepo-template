f2b5b1fc92e9197203e809c4d8420ca6862e9b05
Tin Tran <tintranfs@gmail.com>
2024-06-28 20:29:35 +0700

    Update next app

--- apps/next-app/src/components/headers/topbar.tsx
+++ apps/next-app/src/components/headers/topbar.tsx
@@ -2,10 +2,12 @@
 
 import { FC } from 'react';
 import { Session } from 'next-auth';
+import { Button } from '~react-web-ui-shadcn/components/ui/button';
 
 import Authenticated from '@/modules/auth/components/authenticated';
 import Unauthenticated from '@/modules/auth/components/unauthenticated';
 
+import Logo from '../icons/logo';
 import Languages from '../languages/languages';
 import Menu from '../menus/menu';
 
@@ -15,13 +17,17 @@ interface ITopBarProps {
 
 const TopBar: FC<ITopBarProps> = ({ userSession }) => {
   return (
-    <div className="topbar bg-primary-700 text-white" data-testid="topbar">
-      <div className="flex flex-wrap items-center justify-between">
-        <Menu />
-        <Languages />
-        <div className="authentication ml-6 flex items-center">
-          <Authenticated userSession={userSession} />
-          <Unauthenticated visible={!userSession} />
+    <div className="topbar" data-testid="topbar">
+      <div className="container">
+        <div className="flex flex-wrap items-center justify-between">
+          <Logo />
+          <Menu />
+          <Languages />
+          <div className="authentication ml-6 flex items-center">
+            <Authenticated userSession={userSession} />
+            <Unauthenticated visible={!userSession} />
+          </div>
+          <Button>Get Go Fast</Button>
         </div>
       </div>
     </div>
